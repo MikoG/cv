6d8b68477d5a8e0843183c948e6050223ec6dfe7
MikoG <gethin.o.jones@gmail.com>
2019-05-25 15:21:30 +0100

    first commit

---
+++ src/main/java/com/gjones/profile/interactivecv/InteractivecvApplication.java
@@ -0,0 +1,13 @@
+package com.gjones.profile.interactivecv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractivecvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractivecvApplication.class, args);
+	}
+
+}
